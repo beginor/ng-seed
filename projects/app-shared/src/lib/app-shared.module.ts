bdb1b076c12114a5eef561f50be6a8bcdc41f921
beginor <beginor@qq.com>
2021-01-12 10:48:45 +0800

    backdrop-blur directive

--- projects/app-shared/src/lib/app-shared.module.ts
+++ projects/app-shared/src/lib/app-shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { SvgIconComponent } from './svg-icon/svg-icon.component';
+import { BackdropBlurDirective } from './backdrop-blur/backdrop-blur.directive';
 
 @NgModule({
     declarations: [
-        SvgIconComponent
+        SvgIconComponent,
+        BackdropBlurDirective
     ],
     imports: [
         HttpClientModule
     ],
     exports: [
-        SvgIconComponent
+        SvgIconComponent,
+        BackdropBlurDirective
     ]
 })
 export class AppSharedModule { }
---
+++ projects/app-shared/src/lib/backdrop-blur/backdrop-blur.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BackdropBlurDirective } from './backdrop-blur.directive';
+
+describe('BackdropBlurDirective', () => {
+    it('should create an instance', () => {
+        const directive = new BackdropBlurDirective(null as any);
+        expect(directive).toBeTruthy();
+    });
+});
